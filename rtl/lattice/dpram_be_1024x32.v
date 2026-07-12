8c81f309e6f73becae4910257eb725893e2eff68
m1nl <mateusz@nalewajski.pl>
2026-07-12 15:14:51 +0200

    disable GSR for BRAM
    Reset for BRAM only clears output registers and given how
    BRAM is used in the design, it's unnecessary and may increase
    increase reset line fan-out.

--- rtl/lattice/dpram_be_1024x32.v
+++ rtl/lattice/dpram_be_1024x32.v
@@ -5,7 +5,6 @@
 module dpram_be_1024x32 (
   input  wire           clock,
   input  wire           wren_a,
-  input  wire           reset,
   input  wire [  4-1:0] byteena_a,
   input  wire [ 10-1:0] address_a,
   input  wire [ 32-1:0] data_a,
@@ -18,23 +17,20 @@ module dpram_be_1024x32 (
 );
 
 dp_1024x32 ram(
-	.DataInA(data_a),
-	.DataInB(data_b),
-    .ByteEnA(byteena_a),
-    .ByteEnB(byteena_b),
-	.AddressA(address_a),
-	.AddressB(address_b),
-	.ClockA(clock),
-	.ClockB(clock), 
-    .ClockEnA(1'b1),
-	.ClockEnB(1'b1),
-	.WrA(wren_a),
-	.WrB(wren_b),
-	.ResetA(reset),
-	.ResetB(reset),
-	.QA(q_a),
-	.QB(q_b)
+  .DataInA(data_a),
+  .DataInB(data_b),
+  .ByteEnA(byteena_a),
+  .ByteEnB(byteena_b),
+  .AddressA(address_a),
+  .AddressB(address_b),
+  .ClockA(clock),
+  .ClockB(clock),
+  .ClockEnA(1'b1),
+  .ClockEnB(1'b1),
+  .WrA(wren_a),
+  .WrB(wren_b),
+  .QA(q_a),
+  .QB(q_b)
 );
 
 endmodule
-
